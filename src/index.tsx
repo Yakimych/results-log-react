735b211edf1ff63f6bd3c397b9e1619e2db6ebda
Yakimych <yakimych@gmail.com>
2019-07-09 00:34:03 +0200

    Call graphQL endpoint and log the response

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+
+const endpointUrl = process.env.REACT_APP_ENDPOINT_URL;
+
+const client = new ApolloClient({
+  uri: endpointUrl
+});
+
+client
+  .query({
+    query: gql`
+      {
+        communities(where: { name: { _eq: "anturahockey" } }) {
+          id
+          name
+          description
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 ReactDOM.render(<App />, document.getElementById("root"));
