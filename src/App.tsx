c02d176329e37b16a8f303e87d1b16b42851c64c
Yakimych <yakimych@gmail.com>
2019-07-14 02:17:19 +0200

    Add subscription with hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { AddResult } from "./AddResult";
 import { Results } from "./Results";
+import { ResultCountIndicator } from "./ResultCountIndicator";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <header className="App-header">Result log</header>
       <AddResult />
       <Results />
+      <ResultCountIndicator />
     </div>
   );
 };
