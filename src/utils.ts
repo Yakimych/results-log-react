bb63578de5eee64687d7ac1b84d7e1592014a270
Kyrylo Yakymenko <yakimych@gmail.com>
2019-07-16 21:39:33 +0200

    Always submit result at the time the button was clicked

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,13 @@
 export const formatDate = (date: Date) => date.toISOString().substring(0, 10);
 
+export const withCurrentTime = (date: Date) => {
+  const dateWithCurrentTime = new Date();
+  dateWithCurrentTime.setFullYear(date.getFullYear());
+  dateWithCurrentTime.setMonth(date.getMonth());
+  dateWithCurrentTime.setDate(date.getDate());
+  return dateWithCurrentTime;
+};
+
 export const validNumberOfGoals = (goalsString: string) => {
   const goalsNumber = Number(goalsString);
   return isNaN(goalsNumber) ? 0 : Math.max(Math.floor(goalsNumber), 0);
