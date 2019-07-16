ae44fc5cc03e6f69cdccac260097ee58b2386b5f
Kyrylo Yakymenko <yakimych@gmail.com>
2019-07-16 21:10:12 +0200

    Bake the ResultCountIndicator component into the Results component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import "./App.css";
 import { AddResult } from "./AddResult";
 import { Results } from "./Results";
-import { ResultCountIndicator } from "./ResultCountIndicator";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">Result log</header>
       <AddResult />
-      <ResultCountIndicator />
       <Results />
     </div>
   );
--- src/Results.tsx
+++ src/Results.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useQuery } from "react-apollo-hooks";
+import { useQuery, useSubscription } from "react-apollo-hooks";
 import { ALL_RESULTS_QUERY, ResultsQueryResponse } from "./queries";
 import Table from "@material-ui/core/Table";
 import {
@@ -7,9 +7,12 @@ import {
   TableHead,
   TableRow,
   TableCell,
-  TableBody
+  TableBody,
+  Badge,
+  Button
 } from "@material-ui/core";
 import { formatDate } from "./utils";
+import { ResultCount, RESULT_COUNT_SUBSCRIPTION } from "./subscriptions";
 
 const getPlayerStyle = (isWinningPlayer: boolean): React.CSSProperties => ({
   fontWeight: isWinningPlayer ? "bold" : "normal"
@@ -39,56 +42,86 @@ const containerStyle: React.CSSProperties = {
   width: 650
 };
 
+const communityname = process.env.REACT_APP_COMMUNITY_NAME;
+
 export const Results: React.FC = () => {
-  const { data, error, loading } = useQuery<ResultsQueryResponse>(
-    ALL_RESULTS_QUERY,
-    { variables: { communityname: process.env.REACT_APP_COMMUNITY_NAME } }
+  const allResultsQuery = useQuery<ResultsQueryResponse>(ALL_RESULTS_QUERY, {
+    variables: { communityname }
+  });
+
+  const resultCountSubscription = useSubscription<ResultCount>(
+    RESULT_COUNT_SUBSCRIPTION,
+    { variables: { communityname } }
   );
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error!</p>;
-  if (data === undefined) return <p>Data is undefined</p>;
+  if (allResultsQuery.loading) return <p>Loading...</p>;
+  if (allResultsQuery.error) return <p>Error!</p>;
+  if (allResultsQuery.data === undefined) return <p>Data is undefined</p>;
+
+  const getNewResultsButton = (fetchedResultsCount: number) => {
+    if (
+      resultCountSubscription.loading ||
+      resultCountSubscription.error ||
+      resultCountSubscription.data === undefined
+    )
+      return null;
+
+    const newResultsCount =
+      resultCountSubscription.data.results_aggregate.aggregate.count -
+      fetchedResultsCount;
+
+    return newResultsCount > 0 ? (
+      <Badge color="primary" badgeContent={newResultsCount.toString()}>
+        <Button variant="contained" onClick={_ => allResultsQuery.refetch()}>
+          Refresh Results
+        </Button>
+      </Badge>
+    ) : null;
+  };
 
   return (
-    <Paper style={containerStyle}>
-      <Table style={containerStyle} size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">Player1</TableCell>
-            <TableCell style={goalsStyle}>G1</TableCell>
-            <TableCell style={colonStyle} />
-            <TableCell style={goalsStyle}>G2</TableCell>
-            <TableCell>Player2</TableCell>
-            <TableCell style={extraTimeStyle} align="right">
-              E
-            </TableCell>
-            <TableCell style={dateStyle}>Date</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.results.map(r => {
-            const player1Won = r.player1goals > r.player2goals;
-            const player2Won = !player1Won;
-            const formattedDate = formatDate(new Date(r.date));
+    <>
+      {getNewResultsButton(allResultsQuery.data.results.length)}
+      <Paper style={containerStyle}>
+        <Table style={containerStyle} size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Player1</TableCell>
+              <TableCell style={goalsStyle}>G1</TableCell>
+              <TableCell style={colonStyle} />
+              <TableCell style={goalsStyle}>G2</TableCell>
+              <TableCell>Player2</TableCell>
+              <TableCell style={extraTimeStyle} align="right">
+                E
+              </TableCell>
+              <TableCell style={dateStyle}>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {allResultsQuery.data.results.map(r => {
+              const player1Won = r.player1goals > r.player2goals;
+              const player2Won = !player1Won;
+              const formattedDate = formatDate(new Date(r.date));
 
-            return (
-              <TableRow key={r.id}>
-                <TableCell style={getPlayerStyle(player1Won)} align="right">
-                  {r.player1.name}
-                </TableCell>
-                <TableCell style={goalsStyle}>{r.player1goals}</TableCell>
-                <TableCell style={colonStyle}> : </TableCell>
-                <TableCell style={goalsStyle}>{r.player2goals}</TableCell>
-                <TableCell style={getPlayerStyle(player2Won)}>
-                  {r.player2.name}
-                </TableCell>
-                <TableCell align="right">{r.extratime ? "X" : ""}</TableCell>
-                <TableCell>{formattedDate}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
+              return (
+                <TableRow key={r.id}>
+                  <TableCell style={getPlayerStyle(player1Won)} align="right">
+                    {r.player1.name}
+                  </TableCell>
+                  <TableCell style={goalsStyle}>{r.player1goals}</TableCell>
+                  <TableCell style={colonStyle}> : </TableCell>
+                  <TableCell style={goalsStyle}>{r.player2goals}</TableCell>
+                  <TableCell style={getPlayerStyle(player2Won)}>
+                    {r.player2.name}
+                  </TableCell>
+                  <TableCell align="right">{r.extratime ? "X" : ""}</TableCell>
+                  <TableCell>{formattedDate}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    </>
   );
 };
