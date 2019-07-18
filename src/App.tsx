86fcf9ca07cd7fa1414696a699908d039f4d159a
Yakimych <yakimych@gmail.com>
2019-07-18 07:51:30 +0200

    Show start page when navigating to URL root

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import "./App.css";
 import { AddResult } from "./AddResult";
 import { Results } from "./Results";
+import { getCommunityNameFromUrl } from "./utils";
+import { StartPage } from "./StartPage";
 
 const App: React.FC = () => {
+  const communityName = getCommunityNameFromUrl();
+
   return (
     <div className="App">
       <header className="App-header">Result log</header>
-      <AddResult />
-      <Results />
+      {communityName.length === 0 ? (
+        <StartPage />
+      ) : (
+        <>
+          <AddResult />
+          <Results />
+        </>
+      )}
     </div>
   );
 };
---
+++ src/StartPage.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import createSpacing from "@material-ui/core/styles/createSpacing";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(3, 2)
+    }
+  })
+);
+
+export const StartPage: React.FC = () => {
+  const classes = useStyles(createSpacing(10));
+
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          Welcome to ResultLog
+        </Typography>
+        <Typography component="p">
+          Save all match results in order to analyze, view head-to-head records,
+          analyze stats and more.
+        </Typography>
+        <Typography component="p">
+          Found a bug or missing a feature? File an issue in the{" "}
+          <a href="https://github.com/Yakimych/results-log-react">
+            GitHub repo
+          </a>
+          .
+        </Typography>
+      </Paper>
+    </div>
+  );
+};
