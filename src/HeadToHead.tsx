735e7d59a936be1011ddac7d1c683bcff44d5bd7
Kyrylo Yakymenko <yakimych@gmail.com>
2019-07-20 17:59:10 +0200

    Fetch and show Head to Head results

--- src/HeadToHead.tsx
+++ src/HeadToHead.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
+import { CommunityNameProps } from "./RouteProps";
+import { useQuery } from "react-apollo-hooks";
+import { ResultsQueryResponse, HEAD_TO_HEAD_QUERY } from "./queries";
+import { ResultsTable } from "./ResultsTable";
 
 type Props = {
-  player1Name: string;
-  player2Name: string;
-};
+  player1name: string;
+  player2name: string;
+} & CommunityNameProps;
 
-export const HeadToHead: React.FC<RouteComponentProps<Props>> = props => (
-  <>
-    <h1>Head to Head Record</h1>
-    <div>{props.player1Name}</div>
-    <div>{props.player2Name}</div>
-  </>
-);
+export const HeadToHead: React.FC<RouteComponentProps<Props>> = ({
+  player1name,
+  player2name,
+  communityname
+}) => {
+  const headToHeadQuery = useQuery<ResultsQueryResponse>(HEAD_TO_HEAD_QUERY, {
+    variables: { communityname, player1name, player2name }
+  });
+
+  if (headToHeadQuery.loading) return <p>Loading...</p>;
+  if (headToHeadQuery.error) return <p>Error!</p>;
+  if (headToHeadQuery.data === undefined) return <p>Data is undefined</p>;
+
+  return (
+    <>
+      <h1>Head to Head Record</h1>
+      <div>{player1name}</div>
+      <div>{player2name}</div>
+      <ResultsTable results={headToHeadQuery.data.results} />
+    </>
+  );
+};
---
+++ src/ResultsTable.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Result } from "./queries";
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableHead,
+  Paper
+} from "@material-ui/core";
+import { formatDate } from "./utils";
+
+type Props = {
+  results: readonly Result[];
+  newResults?: readonly Result[];
+};
+
+const getPlayerStyle = (isWinningPlayer: boolean): React.CSSProperties => ({
+  fontWeight: isWinningPlayer ? "bold" : "normal"
+});
+
+const goalsStyle: React.CSSProperties = {
+  width: 20,
+  paddingLeft: 10,
+  paddingRight: 0
+};
+
+const colonStyle: React.CSSProperties = {
+  width: 5,
+  paddingLeft: 0,
+  paddingRight: 0
+};
+
+const dateStyle: React.CSSProperties = {
+  width: 100
+};
+
+const extraTimeStyle: React.CSSProperties = {
+  width: 20
+};
+
+const containerStyle: React.CSSProperties = {
+  width: 650
+};
+
+export const ResultsTable: React.FC<Props> = ({ results, newResults }) => (
+  <>
+    <Paper style={containerStyle}>
+      <Table style={containerStyle} size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">Player1</TableCell>
+            <TableCell style={goalsStyle}>G1</TableCell>
+            <TableCell style={colonStyle} />
+            <TableCell style={goalsStyle}>G2</TableCell>
+            <TableCell>Player2</TableCell>
+            <TableCell style={extraTimeStyle} align="right">
+              E
+            </TableCell>
+            <TableCell style={dateStyle}>Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {results.map(r => {
+            const player1Won = r.player1goals > r.player2goals;
+            const player2Won = !player1Won;
+            const formattedDate = formatDate(new Date(r.date));
+
+            return (
+              <TableRow
+                key={r.id}
+                className={
+                  newResults && newResults.indexOf(r) > -1 ? "highlighted" : ""
+                }
+              >
+                <TableCell style={getPlayerStyle(player1Won)} align="right">
+                  {r.player1.name}
+                </TableCell>
+                <TableCell style={goalsStyle}>{r.player1goals}</TableCell>
+                <TableCell style={colonStyle}> : </TableCell>
+                <TableCell style={goalsStyle}>{r.player2goals}</TableCell>
+                <TableCell style={getPlayerStyle(player2Won)}>
+                  {r.player2.name}
+                </TableCell>
+                <TableCell align="right">{r.extratime ? "X" : ""}</TableCell>
+                <TableCell>{formattedDate}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  </>
+);
