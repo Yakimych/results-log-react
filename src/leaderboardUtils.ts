7feb33a8c80e8555f8b8c0fd08f3022d3ba7174a
Kyrylo Yakymenko <yakimych@gmail.com>
2019-07-27 11:39:03 +0200

    Remove duplication in add_ResultToRow functions

--- src/leaderboardUtils.ts
+++ src/leaderboardUtils.ts
@@ -17,28 +17,16 @@ export type ExtendedLeaderboardRow = LeaderboardRow & {
 
 export const MIN_MATCHES = 2;
 
-const addPlayer1ResultToRow = (row: LeaderboardRow, result: Result) => {
-  const hasWonMatch = result.player1goals > result.player2goals;
+const addResultToRow = (
+  row: LeaderboardRow,
+  player1Goals: number,
+  player2Goals: number
+) => {
+  const hasWonMatch = player1Goals > player2Goals;
   const matchesWon = row.matchesWon + (hasWonMatch ? 1 : 0);
   const matchesLost = row.matchesLost + (hasWonMatch ? 0 : 1);
-  const goalsScored = row.goalsScored + result.player1goals;
-  const goalsConceded = row.goalsConceded + result.player2goals;
-
-  return {
-    ...row,
-    matchesWon,
-    matchesLost,
-    goalsScored,
-    goalsConceded
-  };
-};
-
-const addPlayer2ResultToRow = (row: LeaderboardRow, result: Result) => {
-  const hasWonMatch = result.player2goals > result.player1goals;
-  const matchesWon = row.matchesWon + (hasWonMatch ? 1 : 0);
-  const matchesLost = row.matchesLost + (hasWonMatch ? 0 : 1);
-  const goalsScored = row.goalsScored + result.player2goals;
-  const goalsConceded = row.goalsConceded + result.player1goals;
+  const goalsScored = row.goalsScored + player1Goals;
+  const goalsConceded = row.goalsConceded + player2Goals;
 
   return {
     ...row,
@@ -81,18 +69,26 @@ export const getLeaderboard = (
   const rowMap: Map<string, LeaderboardRow> = new Map<string, LeaderboardRow>();
 
   for (const result of results) {
-    // Player1
+    // Process Player1
     const player1Row =
       rowMap.get(result.player1.name) || emptyRow(result.player1.name);
 
-    const newPlayer1Row = addPlayer1ResultToRow(player1Row, result);
+    const newPlayer1Row = addResultToRow(
+      player1Row,
+      result.player1goals,
+      result.player2goals
+    );
     rowMap.set(result.player1.name, newPlayer1Row);
 
-    // Player2
+    // Process Player2
     const player2Row =
       rowMap.get(result.player2.name) || emptyRow(result.player2.name);
 
-    const newPlayer2Row = addPlayer2ResultToRow(player2Row, result);
+    const newPlayer2Row = addResultToRow(
+      player2Row,
+      result.player2goals,
+      result.player1goals
+    );
     rowMap.set(result.player2.name, newPlayer2Row);
   }
 
