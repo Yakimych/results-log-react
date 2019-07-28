fe20927f8690f7dd1d88c7058e9b44ee4721868d
Kyrylo Yakymenko <yakimych@gmail.com>
2019-07-28 10:44:53 +0200

    Remove unused export

--- src/utils.ts
+++ src/utils.ts
@@ -35,7 +35,7 @@ type WinningLosingResult = {
   extratime: boolean;
 };
 
-export const toWinningLosingResult = (result: Result): WinningLosingResult => {
+const toWinningLosingResult = (result: Result): WinningLosingResult => {
   const player1IsWinner = result.player1goals > result.player2goals;
   if (player1IsWinner) {
     return {
